e3ec6defb5fbc80dbaa1691119c83d8ef7f65a43
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-03-23 13:16:56 -0600

    DAO

---
+++ primerParcial/primerParcial/DAO/ClsDTbl_empleado.cs
@@ -0,0 +1,44 @@
+﻿using primerParcial.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primerParcial.DAO
+{
+    class ClsDTbl_empleado
+    {
+        public List<Tbl_empleado> CargarDatosDeEmpleado()
+        {
+            List<Tbl_empleado> Lista;
+
+
+            using (EMPLEADOEntities db = new EMPLEADOEntities())
+            {
+                Lista = db.Tbl_empleado.ToList();
+            }
+            return Lista;
+        }
+
+        public void DeleteDatosUser(Tbl_empleado Tbl_empleado)
+        {
+            try
+            {
+                using (EMPLEADOEntities db = new EMPLEADOEntities())
+                {
+                    Tbl_empleado = db.Tbl_empleado.Find(Tbl_empleado.Id_empleado);
+                    db.Tbl_empleado.Remove(Tbl_empleado);
+                    db.SaveChanges();
+                    MessageBox.Show("Removido");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.ToString());
+            }
+        }
+    }
+}
